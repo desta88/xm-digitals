e964d8ab8e97f31fbe56180a4edd1942cd755841
desta88 <andhika.permana@salt.co.id>
2026-01-02 18:04:41 +0700

    update sitemap

--- src/app/pages/home/components/hero-dark/hero-dark.component.ts
+++ src/app/pages/home/components/hero-dark/hero-dark.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from "@angular/router";
 
 @Component({
@@ -8,6 +8,18 @@ import { RouterLink } from "@angular/router";
   styleUrl: './hero-dark.component.scss',
   encapsulation: ViewEncapsulation.None
 })
-export class HeroDarkComponent {
-  
+export class HeroDarkComponent implements AfterViewInit {
+  @ViewChild('bgVideo') video!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit() {
+    const v = this.video.nativeElement;
+    v.muted = true;
+
+    const playPromise = v.play();
+    if (playPromise !== undefined) {
+      playPromise.catch(() => {
+        console.warn('Autoplay blocked by browser');
+      });
+    }
+  }
 }
